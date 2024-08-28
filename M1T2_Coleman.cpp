83974a5a79522288f9a0578aa9806e48b9a63fea
leeannakathrine12 <leeannac.212@gmail.com>
2024-08-28 15:16:49 +0000

    M1T2

--- M1T2_Coleman.cpp
+++ M1T2_Coleman.cpp
@@ -1,8 +1,36 @@
 // CSC 134
-// M1LAB - Apple Orhcard
+// M2T1 - Reciept Maker
 // Leeanna Kathrine Coleman
-// 08/26/24
+// 08/28/24
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
+int main() {
+//Variables
+string name;
+double meal_price;
+double tax_rate = 0.08;
+double tax_amount;
+double total_price;
+
+cout << fixed << setprecision(2);
+//Welcome
+cout << "Welcome to the Reciept Maker!\n";
+cout << "What is your name?\n";
+cin >> name;
+cout << "How much was your meal?\n";
+cin >> meal_price;
+
+//Calculations
+tax_amount = meal_price * tax_rate;
+total_price = meal_price + tax_amount;
+
+//Output Reciept
+cout << "Thank you for dining with us " << name << "!\n";
+cout << "Your meal price is $" << meal_price << ".\n";
+cout << "Your tax will be $" << tax_amount << ".\n";
+cout << "Your total price is $" << total_price << ".\n";
+cout << "Please come again!\n";
+}
