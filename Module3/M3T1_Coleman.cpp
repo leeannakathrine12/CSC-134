9896145148e0fa1fa697d189f63e0640936e6858
leeannakathrine12 <leeannac.212@gmail.com>
2024-09-16 14:47:31 +0000

    finished M3T1

--- Module3/M3T1_Coleman.cpp
+++ Module3/M3T1_Coleman.cpp
@@ -11,20 +11,23 @@ int main() {
     double width1, width2, length1, length2;
     double area1, area2;
 
+    //inquiry
     cout << "M3T1 - Area of two rectangles\n";
-    cout << "Width of first rectangle: \n";
+    cout << "Width of first rectangle: ";
     cin >> width1;
-    cout << "Length of first rectangle: \n";
+    cout << "Length of first rectangle: ";
     cin >> length1;
     cout << endl;
-    cout << "Width of second rectangle: \n";
+    cout << "Width of second rectangle: ";
     cin >> width2;
-    cout << "Length of second rectangle: \n";
+    cout << "Length of second rectangle: ";
     cin >> length2;
 
+    // calculations
     area1 = width1 * length1;
     area2 = width2 * length2;
 
-    cout << "The area of the first rectangle is: " + area1;
-    cout << "The area of the second rectangle is: " + area2;
+    // print out answer
+    cout << "The area of the first rectangle is: " << area1 << endl;
+    cout << "The area of the second rectangle is: " << area2 << endl;
 } 
