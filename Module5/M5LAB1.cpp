9455c027a893e6bee3eb57ed4f75a9a3c4379e0d
leeannakathrine12 <leeannac.212@gmail.com>
2024-10-28 15:05:56 +0000

    M5LAB1

--- Module5/M5LAB1.cpp
+++ Module5/M5LAB1.cpp
@@ -7,6 +7,62 @@
 
 using namespace std;
 
+void main_menu();
+void choice_front_door();
+void choice_back_door();
+void choice_go_home();
+
 int main() {
-    
+  cout << "M5LAB1 - Choose Your Own Adventure" << endl;
+  main_menu();
+  cout << "Thanks for playing!" << endl;
+  return 0;
 }
+
+void main_menu() {
+  cout << "Main Menu" << endl;
+  cout << "You're in front of a spooky old house..." << endl;
+  cout << "Do you:" << endl;
+  cout << "1. Try the front door" << endl;
+  cout << "2. Sneak around back" << endl;
+  cout << "3. Forget it, and go home" << endl;
+  cout << "4. [Quit]" << endl;
+  cout << "Choose: ";
+  int choice;
+  cin >> choice;
+  if (1 == choice) {
+    choice_front_door();
+  } else if (2 == choice) {
+
+  } else if (3 == choice) {
+
+  } else if (4 == choice) {
+    cout << "Ok, quitting game" << endl;
+    return;
+  } else {
+    cout << "That's not a valid choice, please try again." << endl;
+    cin.ignore();
+    main_menu();
+  }
+}
+
+
+void choice_front_door() {
+  cout << "Try the front door." << endl;
+  cout << "It's locked. " << endl;
+  cout << "Do you:" << endl;
+  cout << "1. Check around back" << endl;
+  cout << "2. Give up and go home" << endl;
+  int choice;
+  cout << "Choose: ";
+  cin >> choice;
+  if (1 == choice) {
+    choice_back_door();
+  } else if (2 == choice) {
+    choice_go_home();
+  }
+}
+
+void choice_back_door() { cout << "TODO: Write something here" << endl; }
+
+void choice_go_home() { cout << "TODO: Write something here" << endl; }
