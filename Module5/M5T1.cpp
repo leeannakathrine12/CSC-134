e22408f545dcee7f3e3d540639852bcdfee4d2b4
leeannakathrine12 <leeannac.212@gmail.com>
2024-10-28 14:54:05 +0000

    M5T1

--- Module5/M5T1.cpp
+++ Module5/M5T1.cpp
@@ -15,10 +15,10 @@ int main() {
     cout << "Hello from main()" << endl;
     say_hello();
     int my_answer = give_the_answer();
-    cout << "The answer is: " << my_answer << endl;
-
-    int twotimes = double_a_number(7);
-    cout << "Here's another number: " << twotimes << endl;
+    cout << "The magic number is: " << my_answer << endl;
+    int num = my_answer;
+    int twotimes = double_a_number(num);
+    cout << "Double that is: " << twotimes << endl;
     return 0;
 }
 
@@ -28,8 +28,10 @@ void say_hello() {
 }
 
 int give_the_answer() {
-    return 42;
+    int answer = 42;
+    return answer;
 }
+
 int double_a_number(int num) {
     int new_num = num * 2;
     return new_num;
